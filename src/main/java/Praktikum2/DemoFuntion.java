72cb20d2e735cfcd05a172d158711ef701ac4b17
Alfa hardiyansyah <56823267+alfast456@users.noreply.github.com>
2021-04-16 08:49:01 +0700

    Initial commit

---
+++ src/main/java/Praktikum2/DemoFuntion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum2;
+import java.util.Scanner;
+/**
+ *
+ * @author HP
+ */
+public class DemoFuntion {
+    public static void main(String[] args) {
+        drawTriangle(3);
+        drawSquare(5,7);
+        drawSquare(2,2);
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void drawTriangle(int length) {
+        // TODO code application logic here
+        for (int row=1; row<=length; row++){
+            for(int column=1; column<=row; column++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+    }
+    static void drawSquare(int length, int width) {
+        for(int row=1; row<=length; row++){
+            for(int column=1; column<=width; column++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        
+    }
+    
+}
