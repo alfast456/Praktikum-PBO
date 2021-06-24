efc1218b1037258b540573ade8080344cc94fc95
Alfa hardiyansyah <56823267+alfast456@users.noreply.github.com>
2021-06-24 15:27:22 +0700

    Praktikum 7

---
+++ src/main/java/com/nama/praktikumpbo/pertemuan7/guided/Basic.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nama.praktikumpbo.pertemuan7.guided;
+
+/**
+ *
+ * @author HP
+ */
+public class Basic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] myNumbers = {1,2,3};
+        
+        try{
+            System.out.println(myNumbers[10]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Error : Array tidak sebesar itu!");
+        }
+    }
+    
+}
---
+++ src/main/java/com/nama/praktikumpbo/pertemuan7/guided/MultipleCatch.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nama.praktikumpbo.pertemuan7.guided;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class MultipleCatch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        try{
+            System.out.println("Maukkan Bilangan: ");
+            int bil1 = input.nextInt();
+            
+            System.out.println("Masukkan pembagi: ");
+            int bil2 = input.nextInt();
+            
+            int hasil = bil1/bil2;
+            System.out.println(bil1+" : "+ bil2 + " = "+ hasil + " (dibulatkan)");
+        }
+        catch(ArithmeticException e){
+            System.out.println("Error: Tidak dapaat melakukan kalkulasi!");
+        }
+        catch(InputMismatchException e){
+            System.out.println("Error: mohon jangan masukkan selain angka!");
+        }
+    }
+}
